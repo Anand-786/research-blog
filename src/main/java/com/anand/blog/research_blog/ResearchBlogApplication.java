7f3ca6536ad24abc11a2cf9b56b49cac95149ca2
Anand <anandprakash160600@gmail.com>
2025-07-03 04:34:54 +0530

    Initial Commit

---
+++ src/main/java/com/anand/blog/research_blog/ResearchBlogApplication.java
@@ -0,0 +1,13 @@
+package com.anand.blog.research_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchBlogApplication.class, args);
+	}
+
+}
