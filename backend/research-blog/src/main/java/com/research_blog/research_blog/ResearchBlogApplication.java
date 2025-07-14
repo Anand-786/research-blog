379a9c8ccef3e0e0c6281e26c21143703590a7a4
Anand <anandprakash160600@gmail.com>
2025-07-14 12:28:50 +0530

    Starting with Springboot backend

---
+++ backend/research-blog/src/main/java/com/research_blog/research_blog/ResearchBlogApplication.java
@@ -0,0 +1,13 @@
+package com.research_blog.research_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchBlogApplication.class, args);
+	}
+
+}
---
+++ backend/research-blog/src/test/java/com/research_blog/research_blog/ResearchBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research_blog.research_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
